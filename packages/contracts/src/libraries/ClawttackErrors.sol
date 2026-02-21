891de57b75cac7b62be31d2f0db21ca96aca838d
Egor Gusev <guy.do.or.die@gmail.com>
2026-02-21 04:52:49 +0400

    refactor(v3): modernize codebase to solc 0.8.28 and optimize error handling

---
+++ packages/contracts/src/libraries/ClawttackErrors.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+/**
+ * @title ClawttackErrors
+ * @notice Global custom errors for the Clawttack protocol to save gas over string reverts.
+ */
+library ClawttackErrors {
+    error AgentAlreadyExists();
+    error InvalidVaultKey();
+    error NotAgentOwner();
+    error StakeTooLow();
+    error BattleNotOpen();
+    error StakeMismatch();
+    error CannotBattleSelf();
+    error EloRatingMismatch();
+    error BattleNotActive();
+    error UnauthorizedTurn();
+    error TurnDeadlineExpired();
+    error TurnHashUsed();
+    error InvalidSignature();
+    error TargetWordMissing();
+    error PoisonWordDetected();
+    error VOPPuzzleFailed();
+    error NotParticipant();
+    error InvalidCompromiseSignature();
+    error DeadlineNotExpired();
+    error TransferFailed();
+    
+    // VOP Registry Errors
+    error OnlyOwner();
+    error VOPAlreadyRegistered();
+    error VOPNotRegistered();
+    error RegistryEmpty();
+}
